fd204528398ae17a458a48a3b59ef445e18dbf14
branmeco <branmeco17@gmail.com>
2023-04-05 20:05:08 -0500

    Turno de la computadora

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,7 +14,9 @@ let puntosComputadora = 0;
 
 //Html references
 const btnPedir = document.querySelector('#btnPedir');
+const btnDetener = document.querySelector('#btnDetener');
 
+const divCartasComputadora = document.querySelector('#computadora-cartas');
 const divCartasJugador = document.querySelector('#jugador-cartas')
 const puntosHTML = document.querySelectorAll('small');
 
@@ -57,6 +59,27 @@ const valorCarta = (carta) => {
 
 }
 
+//Turno de la computadora
+const turnoComputadora = (puntosMinimos) => {
+    do {
+        const carta = pedirCarta();
+
+        puntosComputadora = puntosComputadora + valorCarta(carta);
+        puntosHTML[1].innerText = puntosComputadora;
+
+        // <img class="carta" src="assets/cartas/2C.png">
+        const imgCarta = document.createElement('img');
+        imgCarta.src = `assets/cartas/${carta}.png`; //3H, JD
+        imgCarta.classList.add('carta');
+        divCartasComputadora.append(imgCarta);
+
+        if (puntosMinimos > 21) {
+            break;
+        }
+
+    } while ((puntosComputadora < puntosMinimos) && (puntosMinimos <= 21));
+}
+
 //Events
 btnPedir.addEventListener('click', () => {
     const carta = pedirCarta();
@@ -70,9 +93,20 @@ btnPedir.addEventListener('click', () => {
     divCartasJugador.append(imgCarta);
 
     if (puntosJugador > 21) {
+        console.warn('Lo siento mucho, perdiste');
         btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoComputadora(puntosJugador);
     } else if (puntosJugador === 21) {
         console.warn('21, Genial');
         btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoComputadora(puntosJugador);
     }
+});
+
+btnDetener.addEventListener('click', () => {
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+    turnoComputadora(puntosJugador);
 });
